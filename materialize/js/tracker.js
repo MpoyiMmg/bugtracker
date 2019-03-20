4daaedc1e1ac9a1326529fdd63fbc0c330794f53
= <=>
2019-03-21 00:14:57 +0200

    [MAKE] the specification for login

--- materialize/js/tracker.js
+++ materialize/js/tracker.js
@@ -1,6 +1,13 @@
 // ===========================================================>
 
-function login(){
+function login(){  
+    let username = document.querySelector("#username").value
+    let pwd      = document.querySelector("#pwd").value
 
-    
+    if( username.trim() != "admin" || pwd.trim() != "admin" || username.trim() == "" || pwd.trim() == ""){
+        alert("veuillez verifier vos entrees")
+    }
+    else{
+        location.href = "liste_bug.html"
+    }
 }
