503cdd76933ad633ce11d9586847874ab477c88e
= <=>
2019-03-21 00:29:07 +0200

    [GET] Get formular values

--- materialize/js/tracker.js
+++ materialize/js/tracker.js
@@ -4,10 +4,18 @@ function login(){
     let username = document.querySelector("#username").value
     let pwd      = document.querySelector("#pwd").value
 
-    if( username.trim() != "admin" || pwd.trim() != "admin" || username.trim() == "" || pwd.trim() == ""){
+    if( username.trim() != "admin" || pwd.trim() != "admin" || 
+                    username.trim() == "" || pwd.trim() == ""){
         alert("veuillez verifier vos entrees")
     }
     else{
         location.href = "liste_bug.html"
     }
+}
+
+// ================================================================>
+function enregistrer(){
+    let nom      = document.querySelector("#nom").value
+    let descript = document.querySelector("#description").value
+    let lien     = document.querySelector("#lien").value
 }
